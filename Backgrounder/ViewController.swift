73f4d488f7b371c64c2e3206bf86632ef36f1c5d
Alex Agapov <agapov.one@gmail.com>
2017-12-05 22:41:22 +0500

    Initial Commit

---
+++ Backgrounder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Backgrounder
+//
+//  Created by Alex Agapov on 05/12/2017.
+//  Copyright © 2017 Alex Agapov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
