68709fd38dc98f21a6ca5c6a27ff561f0db7683f
Alex Agapov <agapov.one@gmail.com>
2017-12-12 10:03:29 +0500

    Separate viewmodel out of viewcontroller

--- Backgrounder/ViewControllers/PhotosViewController.swift
+++ Backgrounder/ViewControllers/PhotosViewController.swift
@@ -13,8 +13,10 @@ import RxCocoa
 import Moya
 
 final class PhotosViewController: UIViewController, StoryboardSceneBased {
+    // MARK: - Protocols
     static let sceneStoryboard = UIStoryboard(name: "Main", bundle: nil)
 
+    // MARK: - UI Outlets
     @IBOutlet private var collectionView: PhotoCollectionView! {
         didSet {
             collectionView.register(cellType: PhotoCell.self)
@@ -22,56 +24,63 @@ final class PhotosViewController: UIViewController, StoryboardSceneBased {
     }
     private lazy var control = UIRefreshControl()
 
-    private let photos = Variable<[Photo]>([])
-    
+    // MARK: - Properties
     private let disposeBag = DisposeBag()
-
+    
+    private var viewModel: PhotosViewModel! {
+        didSet {
+            setupViewModel()
+        }
+    }
+    
+    // MARK: - Lifecycle
+    static func instantiate(viewModel: PhotosViewModel) -> PhotosViewController {
+        let vc = PhotosViewController.instantiate()
+        vc.viewModel = viewModel
+        return vc
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        photos.asObservable().subscribe { [weak self] event in
-            self?.collectionView.reloadSections(IndexSet(integer: 0))
-            self?.control.endRefreshing()
-        }.disposed(by: disposeBag)
-        
-        control.addTarget(self, action: #selector(refresh), for: .valueChanged)
-        collectionView.refreshControl = control
-        
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(refresh))
-
-        refresh()
+        setupUI()
     }
     
-    @objc private func refresh() {
-        Provider.default.rx
-            .request(.photos(page: 1))
-            .map(Array<Photo>.self)
-            .subscribe { (event) in
-            switch event {
-            case .error(let err):
-                switch err as? MoyaError {
-                case .underlying(let err, let res)?:
-                    print((err, res))
-                default:
-                    print("Unknown error\n\n \(err)")
-                }
-                self.photos.value = []
-            case .success(let result):
-                self.photos.value = result
+    private func setupUI() {
+        title = "Latest"
+        
+        collectionView.refreshControl = control
+    }
+    
+    private func setupViewModel() {
+        viewModel.repositories
+            .observeOn(MainScheduler.instance)
+            .do(onNext: { [weak self] _ in
+                self?.control.endRefreshing()
+            })
+            .subscribe() { photos in
+                self?.collectionView.reloadSections(IndexSet(integer: 0))
             }
-        }.disposed(by: disposeBag)
+            .disposed(by: disposeBag)
+        
+        let item = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: nil)
+        item.rx.tap
+            .bind(to: viewModel.reload)
+            .disposed(by: disposeBag)
+        navigationItem.rightBarButtonItem = item
     }
 }
 
+// MARK: - Collection view
 extension PhotosViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return photos.value.count
+        return viewModel.photos.value.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell: PhotoCell = collectionView.dequeueReusableCell(for: indexPath)
         
-        cell.photo = photos.value[indexPath.row]
+        cell.photo = viewModel.photos.value[indexPath.row]
         return cell
     }
 }
