605718010d32e590b51b16780487b7724b517008
Alex Agapov <agapov.one@gmail.com>
2017-12-12 23:03:55 +0500

    Add photo service with network requests

---
+++ Backgrounder/Networking/PhotoService.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoService.swift
+//  Backgrounder
+//
+//  Created by Alex Agapov on 12/12/2017.
+//  Copyright © 2017 Alex Agapov. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class PhotoService {
+    func getPhotos(page: Int, perPage: Int = 40, orderBy: OrderBy = .latest) -> Observable<[Photo]> {
+        return Provider.default.rx
+            .request(.photos(page: page, perPage: perPage, orderBy: orderBy))
+            .map(Array<Photo>.self)
+            .asObservable()
+    }
+}
