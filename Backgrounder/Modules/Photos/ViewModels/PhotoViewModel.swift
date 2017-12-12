738c45d43b7c816abd5736a787de721bd32705f3
Alex Agapov <agapov.one@gmail.com>
2017-12-12 23:03:13 +0500

    Add photoviewmodel for next features

---
+++ Backgrounder/Modules/Photos/ViewModels/PhotoViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  PhotoViewModel.swift
+//  Backgrounder
+//
+//  Created by Alex Agapov on 12/12/2017.
+//  Copyright © 2017 Alex Agapov. All rights reserved.
+//
+
+import UIKit
+
+// Reserved for proper view model
