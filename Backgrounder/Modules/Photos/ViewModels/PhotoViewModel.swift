c246c23ae51c7b4a5b08211e82318c1839d55911
Alex Agapov <agapov.one@gmail.com>
2017-12-19 22:33:15 +0500

    Add photo view model

--- Backgrounder/Modules/Photos/ViewModels/PhotoViewModel.swift
+++ Backgrounder/Modules/Photos/ViewModels/PhotoViewModel.swift
@@ -6,6 +6,16 @@
 //  Copyright © 2017 Alex Agapov. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
-// Reserved for proper view model
+class PhotoViewModel {
+    let author: String
+    let url: URL
+    let fullImageURL: URL
+    
+    init(photo: Photo) {
+        self.author = "\(photo.user.username) \(photo.user.name)"
+        self.url = photo.urls.thumbnail
+        self.fullImageURL = photo.urls.full
+    }
+}
