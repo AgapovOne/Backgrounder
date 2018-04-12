5e8628fb5bbf3631483ab7c146b3ecac95db7a79
Aleksey Agapov <aleksey.agapov@saritasa.com>
2018-04-12 11:49:58 +0500

    Add deep links

---
+++ Backgrounder/UI/DeepLink.swift
@@ -0,0 +1,14 @@
+//
+//  DeepLink.swift
+//  Backgrounder
+//
+//  Created by Aleksey Agapov on 12/04/2018.
+//  Copyright © 2018 Alex Agapov. All rights reserved.
+//
+
+import Foundation
+
+enum DeepLink {
+    case onboarding
+    case photos
+}
